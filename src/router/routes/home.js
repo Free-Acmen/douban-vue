1cd7024a59451a19a15d179f7d702dedb5635350
FreeAcmen <lmllsq@126.com>
2017-05-15 16:31:18 +0800

    首页路由数据展示bug修复

--- src/router/routes/home.js
+++ src/router/routes/home.js
@@ -1,19 +1,19 @@
 import Home from '../../pages/home'
 import store from '../../store'
 import type from '../../store/mutation-type'
-import {hotMovie} from '../../store/data'
+import { hotMovie } from '../../store/data'
 
 export default {
-    path:'/home',
-    component:Home,
-    beforeEnter:function(to, before, next){
-        if(Object.keys(store.state.home.homeData).length !== 0){
-            store.commit(type.LOADING_FLAG, false)
-            next()
-            return
-        }
+    path: '/home',
+    component: Home,
+    beforeEnter: function(to, before, next) {
+        // if(Object.keys(store.state.home.homeData).length !== 0){
+        //     store.commit(type.LOADING_FLAG, false)
+        //     next()
+        //     return
+        // }
         store.commit(type.LOADING_FLAG, true)
-        hotMovie(3, 0).then(function(homeData){
+        hotMovie(3, 0).then(function(homeData) {
             store.commit(type.HOME_DATA, homeData)
             store.commit(type.LOADING_FLAG, false)
             store.commit(type.NET_STATUS, '')
